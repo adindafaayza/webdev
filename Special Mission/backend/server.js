79bc53dbb0b962096defb7b04c77aa6bcefb03cb
adindafaayza <afm243.sch@gmail.com>
2023-09-12 22:12:11 +0700

    Add Special Mission

---
+++ Special Mission/backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "crud"
+})
+
+app.get("/", (req, res) => {
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("listening");
+})
---
+++ Special Mission/frontend/src/Student.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Student() {
+    return (
+        <div className='d-flex vh-100 justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded'>
+                <button className='btn btn-success'></button>
+            </div>
+        </div>
+    )
+}
+
+export default Student
